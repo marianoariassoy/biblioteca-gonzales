9636d33a7d3a6ec946cfb598cf79c516a120cbad
marianoariassoy <hola@marianoarias.soy>
2024-04-19 12:28:17 -0300

    first commit

---
+++ error/index.tsx
@@ -0,0 +1,15 @@
+import Layout from '../../layout/Layout'
+
+const index = () => {
+  return (
+    <Layout>
+      <section className='section-main'>
+        <div className='w-full max-w-6xl m-auto px-6'>
+          <h1 className='font-secondary text-2xl lg:text-4xl font-bold color-green-1'>Error</h1>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default index
